2b3ba49bc364b0abe915853384b363e8e710cc65
EZEQUIEL IAIR BERAKLA <48715745@ort>
2024-08-22 16:15:20 -0300

    EchuUltimo

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -21,47 +21,32 @@ public class HomeController : Controller
 
     public IActionResult Deportes()
     {
-<<<<<<< HEAD
         List<Deporte> deportes = BD.ListarDeporte();
         ViewBag.Deportes = deportes;
         return View();
-=======
-        ViewBag.ListaDeportes = BD.ListarDeportes();
-        return View("Deportes");
->>>>>>> a165bd25250c09c6376ca0f0c6218fb6ddcb724e
     }
 
     public IActionResult Paises()
-<<<<<<< HEAD
     { 
         List<Pais> paises = BD.ListarPaises();
         ViewBag.Paises = paises;
         return View();
-=======
     {
         ViewBag.ListaPaises = BD.ListarPaises();
         return View("Paises");
->>>>>>> a165bd25250c09c6376ca0f0c6218fb6ddcb724e
     }
 
     public IActionResult VerDetalleDeporte(int idDeporte)
     {
-<<<<<<< HEAD
         Deporte deporte = BD.VerInfoDeporte(idDeporte);
         List<Deportista> deportistas = BD.ListarDeportistas1(idDeporte);
         ViewBag.Deporte = deporte;
         ViewBag.Deporistas = deportistas;
         return View(DetalleDeporte);
-=======
-        ViewBag.Deporte = BD.VerInfoDeporte(idDeporte);
-        ViewBag.Deportistas = BD.ListarDeportistas(idDeporte);
-        return View("DetalleDeporte");
->>>>>>> a165bd25250c09c6376ca0f0c6218fb6ddcb724e
     }
 
     public IActionResult VerDetallePais(int idPais)
     {
-<<<<<<< HEAD
         Pais pais = BD.VerInfoDeporte(idPais);
         List<Deportista> deportistas = BD.ListarDeportistas1(idPais);
         ViewBag.Pais = pais;
@@ -92,12 +77,10 @@ public class HomeController : Controller
         return View(Creditos);
     }
 }
-=======
         ViewBag.Pais = BD.VerInfoPais(idPais);
         ViewBag.Deportistas = BD.ListarDeportistas1(idPais);
-        return View("DetallePais");
+        return View(DetallePais);
     }
->>>>>>> a165bd25250c09c6376ca0f0c6218fb6ddcb724e
 
     public IActionResult VerDetalleDeportista(int idDeportista)
     {
